20ca37c2eb0f6d9889d197d4f1283bd6f9d7e82c
Jose Rodriguez <jose.rodriguez@avantica.net>
2019-08-22 19:37:27 -0500

    adding production files only for educational porpuses

---
+++ build/precache-manifest.30327b7a8c17c9e0f300475eccf5102f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f727bb55d303c6e4fc39fd56a672c37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3901c2131cf9771dfd7c",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "bfc198c408177749e351",
+    "url": "/static/css/main.353a461a.chunk.css"
+  },
+  {
+    "revision": "3901c2131cf9771dfd7c",
+    "url": "/static/js/2.03a19453.chunk.js"
+  },
+  {
+    "revision": "bfc198c408177749e351",
+    "url": "/static/js/main.a3bc8868.chunk.js"
+  },
+  {
+    "revision": "85304a0d8147cfb7967e",
+    "url": "/static/js/runtime~main.077bb605.js"
+  }
+]);
